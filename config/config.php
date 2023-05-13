0b7d7ab3ad8c82047b50070454c1f594ddad019a
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-13 18:00:18 +0200

    Defined routes and setted dynamically. Included files to headers

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Definir las rutas del proyecto
+
+    // Admin
+    define('RUTA_ADMIN', 'http://localhost/blog/admin/');
+
+    // Front
+    define('RUTA_FRONT', 'http://localhost/blog/');
--- includes/header.php
+++ includes/header.php
@@ -1,81 +1,91 @@
+<?php
+include '../config/config.php';
+include '../config/basemysql.php';
+include '../helpers/helper_format.php';
+include '../models/articulo.php';
+include '../models/usuario.php';
+include '../models/comentario.php';
+?>
+
 <!doctype html>
 <html lang="es">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../css/bootstrap-icons-1.2.1/font/bootstrap-icons.css">
 
-    <link href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" rel="stylesheet">    
+    <link href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="../css/estilos.css">
 
     <title>Blog PHP 8</title>
-  </head>
-  <body>
-    
+</head>
+
+<body>
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Blog PHP 8</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <a class="navbar-brand" href="<?php echo RUTA_FRONT ?>">Blog PHP 8</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
-                
-             
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             Administración
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li>
-                            <a class="dropdown-item" href="articulos.php">Artículos</a>
+                                <a class="dropdown-item" href="<?php echo RUTA_ADMIN ?>articulos.php">Artículos</a>
                             </li>
                             <li>
-                            <a class="dropdown-item" href="comentarios.php">Comentarios</a>
-                            </li>                        
+                                <a class="dropdown-item" href="<?php echo RUTA_ADMIN ?>comentarios.php">Comentarios</a>
+                            </li>
                         </ul>
                     </li>
 
-                    
+
 
                     <li class="nav-item">
-                            <a class="nav-link" href="usuarios.php">Usuarios</a>
-                      </li>  
-                      
+                        <a class="nav-link" href="<?php echo RUTA_ADMIN ?>usuarios.php">Usuarios</a>
+                    </li>
+
                 </ul>
 
-                <ul class="navbar-nav mb-2 mb-lg-0">                       
-                        <li class="nav-item">
-                            <a class="nav-link" href="../index.php">Inicio</a>
-                        </li>
-                      
-                            <li class="nav-item">
-                                <a class="nav-link" href="../registro.php">Registrarse</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../acceder.php">Acceder</a>
-                            </li>
-                      
-                         
-                          <li class="nav-item">
-                              <p class="text-white mt-2"><i class="bi bi-person-circle"></i></p>
-                          </li>
-                          <li class="nav-item">
-                                <a class="nav-link" href="../salir.php">Salir</a>
-                            </li>  
-                                
-                    </ul> 
-                      
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo RUTA_FRONT ?>registro.php">Registrarse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo RUTA_FRONT ?>acceder.php">Acceder</a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <p class="text-white mt-2"><i class="bi bi-person-circle"></i></p>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo RUTA_FRONT ?>salir.php">Salir</a>
+                    </li>
+
+                </ul>
+
             </div>
         </div>
-    </nav>  
+    </nav>
 
     <div class="container mt-5 caja">
-       
