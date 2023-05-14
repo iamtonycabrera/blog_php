2d086b5eb71684ad24f126c9cd43427dc0ec4a49
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-14 13:18:13 +0200

    Implemented read the category by id and filled inputs for editing

--- admin/editar_articulo.php
+++ admin/editar_articulo.php
@@ -1,45 +1,68 @@
-<?php include("../includes/header.php") ?>
+<?php
+include("../includes/header.php");
+
+// Instanciar db y conn
+$baseDatos = new Basemysql();
+$db = $baseDatos->connect();
+
+$id = null;
+
+// Validar si se envio el id
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+
+// Instanciar el objeto articulo
+$articulos = new Articulo($db);
+$resultado = $articulos->leer_individual($id);
+
+
+?>
 
 <div class="row">
-       
-    </div>
+
+</div>
 
 <div class="row">
-        <div class="col-sm-6">
-            <h3>Editar Artículo</h3>
-        </div>            
+    <div class="col-sm-6">
+        <h3>Editar Artículo</h3>
     </div>
-    <div class="row">
-        <div class="col-sm-6 offset-3">
+</div>
+<div class="row">
+    <div class="col-sm-6 offset-3">
         <form method="POST" action="" enctype="multipart/form-data">
 
             <input type="hidden" name="id" value="4">
 
             <div class="mb-3">
                 <label for="titulo" class="form-label">Título:</label>
-                <input type="text" class="form-control" name="titulo" id="titulo" value="titulo test">               
+                <input type="text" class="form-control" name="titulo" id="titulo"
+                    value="<?php echo $resultado->titulo ?>">
             </div>
 
             <div class="mb-3">
-                <img class="img-fluid img-thumbnail" src="../img/articulos/img4.jpg">
+                <img class="img-fluid img-thumbnail" src="<?php echo RUTA_FRONT . "img/articulos/" . $resultado->imagen ?>">
             </div>
 
             <div class="mb-3">
                 <label for="imagen" class="form-label">Imagen:</label>
-                <input type="file" class="form-control" name="imagen" id="imagen" placeholder="Selecciona una imagen">               
+                <input type="file" class="form-control" name="imagen" id="imagen" placeholder="Selecciona una imagen">
             </div>
             <div class="mb-3">
-                <label for="texto">Texto</label>   
-                <textarea class="form-control" placeholder="Escriba el texto de su artículo" name="texto" style="height: 200px">
-               ejemplo texto
-                </textarea>              
-            </div>          
-        
-            <br />
-            <button type="submit" name="editarArticulo" class="btn btn-success float-left"><i class="bi bi-person-bounding-box"></i> Editar Artículo</button>
+                <label for="texto">Texto</label>
+                <textarea class="form-control" placeholder="Escriba el texto de su artículo" name="texto"
+                    style="height: 200px">
+                <?php echo $resultado->texto ?>
+                </textarea>
+            </div>
 
-            <button type="submit" name="borrarArticulo" class="btn btn-danger float-right"><i class="bi bi-person-bounding-box"></i> Borrar Artículo</button>
-            </form>
-        </div>
+            <br />
+            <button type="submit" name="editarArticulo" class="btn btn-success float-left"><i
+                    class="bi bi-person-bounding-box"></i> Editar Artículo</button>
+
+            <button type="submit" name="borrarArticulo" class="btn btn-danger float-right"><i
+                    class="bi bi-person-bounding-box"></i> Borrar Artículo</button>
+        </form>
     </div>
+</div>
 <?php include("../includes/footer.php") ?>
