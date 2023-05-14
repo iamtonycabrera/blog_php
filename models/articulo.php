f83fbe3a9cb29b1e9dc23f5fb4ae5d648e1446b7
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-14 13:16:47 +0200

    Added read a category method and setted de db table

--- models/articulo.php
+++ models/articulo.php
@@ -2,7 +2,7 @@
 
     class Articulo{
         private $conn;
-        private $table;
+        private $table = 'articulos';
 
         // Propiedades
         public $id;
@@ -29,5 +29,23 @@
             // Retornar los articulo
             $articulos = $stmt->fetchAll(PDO::FETCH_OBJ);
             return $articulos;
+        }
+
+        // Obtener un articulos
+        public function leer_individual($id){
+            $query = 'SELECT id, titulo, imagen, texto, fecha_creacion FROM ' . $this->table . ' WHERE id = ? LIMIT 0,1';
+
+            // Preparar la sentencia
+            $stmt = $this->conn->prepare($query);
+
+            // Vincular parametro
+            $stmt->bindParam(1, $id);
+
+            // Ejecutar query
+            $stmt->execute();
+            
+            // Retornar los articulo
+            $articulo = $stmt->fetch(PDO::FETCH_OBJ);
+            return $articulo;
         }
     }
