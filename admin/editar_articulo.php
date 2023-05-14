1005e075ab24380fdee8089ca32f856262f95e1c
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-14 15:12:16 +0200

    Implemented edit and delete article

--- admin/editar_articulo.php
+++ admin/editar_articulo.php
@@ -14,11 +14,93 @@ if (isset($_GET['id'])) {
 $articulos = new Articulo($db);
 $resultado = $articulos->leer_individual($id);
 
+// Actualizar articulo
+// Validar si se presiono el boton
+if (isset($_POST['editarArticulo'])) {
+    // Obtener los valores
+    $idArticulo = $_POST['id'];
+    $titulo = $_POST['titulo'];
+    $texto = $_POST['texto'];
+
+    if ($_FILES['imagen']['error'] > 0) {
+        // No se sube imagen pero deja actualizar demas campos
+        if (empty($titulo) || $titulo == "" || empty($texto) || $texto == "") {
+            $error = "Algunos campos estan vacios";
+        } else {
+            // Si entra aqui es porque se enviaron todos los campos
+            // Instanciar articulo
+            $articulo = new Articulo($db);
+
+            $newImageName = "";
+
+            if ($articulo->actualizar($idArticulo, $titulo, $newImageName, $texto)) {
+                $mensaje = "Articulo modificado correctamente";
+                header("Location:articulos.php?mensaje=" . urlencode($mensaje));
+                exit();
+            } else {
+                $error = "Articulo no modificado";
+                header("Location:articulos.php?error=" . urlencode($mensaje));
+                exit();
+            }
+        }
+    } else {
+        // Si entra es porque si se subio la imagen
+        // Validar los demas campos
+        if (empty($titulo) || $titulo == "" || empty($texto) || $texto == "") {
+            $error = "Algunos campos estan vacios";
+        } else {
+            // Si entra aqui es porque se enviaron todos los campos
+            // Subida de archivos
+            $image = $_FILES['imagen']['name'];
+            $image_arr = explode('.', $image);
+            $rand = rand(1000, 99999);
+            $newImageName = $image_arr[0] . $rand . '.' . $image_arr[1];
+            $ruta_final = "../img/articulos/" . $newImageName;
+            move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta_final);
+
+            // Instanciar articulo
+            $articulo = new Articulo($db);
+
+            if ($articulo->actualizar($idArticulo, $titulo, $newImageName, $texto)) {
+                $mensaje = "Articulo modificado correctamente";
+                header("Location:articulos.php?mensaje=" . urlencode($mensaje));
+            } else {
+                $error = "Articulo no modificado";
+                header("Location:articulos.php?error=" . urlencode($mensaje));
+            }
+        }
+    }
+}
+
+if (isset($_POST['borrarArticulo'])){
+    // Obtener el id
+    $idArticulo = $_POST['id'];
+    
+    // Instanciar articulo
+    $articulo = new Articulo($db);
+
+    if ($articulo->borrar($idArticulo)) {
+        $mensaje = "Articulo borrado correctamente";
+        header("Location:articulos.php?mensaje=" . urlencode($mensaje));
+    } else {
+        $error = "Error al borrar el articulo";
+    }
+}
+
 
 ?>
 
 <div class="row">
-
+    <div class="col-sm-12">
+        <?php if (isset($error)): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>
+                    <?php echo $error ?>
+                </strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif ?>
+    </div>
 </div>
 
 <div class="row">
@@ -30,7 +112,7 @@ $resultado = $articulos->leer_individual($id);
     <div class="col-sm-6 offset-3">
         <form method="POST" action="" enctype="multipart/form-data">
 
-            <input type="hidden" name="id" value="4">
+            <input type="hidden" name="id" value="<?php echo $resultado->id ?>">
 
             <div class="mb-3">
                 <label for="titulo" class="form-label">Título:</label>
--- models/articulo.php
+++ models/articulo.php
@@ -69,4 +69,62 @@
             // Si hay error
             printf("Error: ", $stmt->error);
         }
+
+        // Actualizar un articulo
+        public function actualizar($id, $titulo, $imagen, $texto){
+
+            if($imagen == ""){
+
+                $query = 'UPDATE ' . $this->table . ' SET titulo = :titulo, texto = :texto WHERE id = :id';
+
+                // Preparar la sentencia
+                $stmt = $this->conn->prepare($query);
+
+                // Vincular parametro
+                $stmt->bindParam(':titulo', $titulo, PDO::PARAM_STR);
+                $stmt->bindParam(':texto', $texto, PDO::PARAM_STR);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+                // Ejecutar query
+                if ($stmt->execute()) {
+                    return true;
+                }
+            } else {
+                $query = 'UPDATE ' . $this->table . ' SET titulo = :titulo, texto = :texto, imagen = :imagen WHERE id = :id';
+
+                // Preparar la sentencia
+                $stmt = $this->conn->prepare($query);
+
+                // Vincular parametro
+                $stmt->bindParam(':titulo', $titulo, PDO::PARAM_STR);
+                $stmt->bindParam(':texto', $texto, PDO::PARAM_STR);
+                $stmt->bindParam(':imagen', $imagen, PDO::PARAM_STR);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+                // Ejecutar query
+                if ($stmt->execute()) {
+                    return true;
+                }
+            }
+            // Si hay error
+            printf("Error: ", $stmt->error);
+        }
+
+        public function borrar($idArticulo){
+            $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+            // Preparar la sentencia
+            $stmt = $this->conn->prepare($query);
+
+            // Vincular parametro
+            $stmt->bindParam(':id', $idArticulo, PDO::PARAM_INT);
+
+            // Ejecutar query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // Si hay error
+            printf("Error: ", $stmt->error);
+        }
     }
