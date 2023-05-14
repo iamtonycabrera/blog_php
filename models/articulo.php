856766a87f9c81b9b1ac6c2079308dc3ae49fc82
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-14 14:04:48 +0200

    Implemented create article method

--- models/articulo.php
+++ models/articulo.php
@@ -48,4 +48,25 @@
             $articulo = $stmt->fetch(PDO::FETCH_OBJ);
             return $articulo;
         }
+
+        // Crear un nuevo articulo
+        public function crear($titulo, $imagen, $texto){
+            $query = 'INSERT INTO ' . $this->table . ' (titulo, imagen, texto)VALUES(:titulo, :imagen, :texto)';
+
+            // Preparar la sentencia
+            $stmt = $this->conn->prepare($query);
+
+            // Vincular parametro
+            $stmt->bindParam(':titulo', $titulo, PDO::PARAM_STR);
+            $stmt->bindParam(':imagen', $imagen, PDO::PARAM_STR);
+            $stmt->bindParam(':texto', $texto, PDO::PARAM_STR);
+
+            // Ejecutar query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // Si hay error
+            printf("Error: ", $stmt->error);
+        }
     }
